cf3388d99e3cf9085f8da0e76a2f99531b5c9d2d
Marijus Laucevicius <marijuslau@yahoo.com>
2018-11-08 15:39:30 +0200

    2 task's 2nd part completed

--- 2/db2.pl
+++ 2/db2.pl
@@ -36,22 +36,27 @@ yraJaunesnisToPacioKurso(StudA, StudB) :-
     studentas(StudB, K2),
     K1 = K2,
     yraJaunesnis(StudA, StudB).
-% 5.1 dalyba (div). naudojant tik sudėties ir atimties operacijas:
 
-%div(0, _Y, 0).
-%div(X, Y, R) :-
-%    X < 0,
-%    Y < 0,
-%    X0 is X - Y,
-%    div(X0, Y, R0),
-%    R is R0+1.
-%div(X, Y, R) :-
-%    X > 0,
-%    X0 is X - Y,
-%    div(X0, Y, R0),
-%    R is R0+1.
-%div(X, Y, R) :-
-%    X < 0,
-%    X0 is X + Y,
-%    div(X0, Y, R0),
-%    R is R0-1.
+% Natūralieji skaičiai yra modeliuojami termais nul, s(nul), s(s(nul)),… Apibrėžkite predikatus:
+% 6.1 „skaičius yra dviejų laipsnis.“
+
+% s(nul) = 1
+% s(s(nul)) = 2
+% s(s(s(s(nul)))) = 4
+% s(s(s(s(s(s(s(s(nul)))))))) = 8
+% s(s(s(s(s(s(s(s(s(s(nul)))))))))) = 10
+% s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(nul)))))))))))))))) = 16
+
+skDviejuLaipsnis(nul) :- false.
+skDviejuLaipsnis(s(nul)).
+skDviejuLaipsnis(X) :-
+    issidalinaIkiDvieju(X, s(s(nul)), Ats),
+    skDviejuLaipsnis(Ats).
+
+suma(nul,X,X).
+suma(s(X),Y,s(Z)):- suma(X,Y,Z).
+
+issidalinaIkiDvieju(s(s(nul)), s(s(nul)), s(nul)).
+issidalinaIkiDvieju(X, s(s(nul)), s(Ats)):- 
+    suma(T, s(s(nul)), X),
+    issidalinaIkiDvieju(T, s(s(nul)), Ats).
