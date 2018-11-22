7b5ddb75480e40205f5ffdc007deea81b93e57e0
Marijus Laucevicius <marijuslau@yahoo.com>
2018-11-22 14:07:05 +0200

    Refactor naming task 3

--- 3/db3.pl
+++ 3/db3.pl
@@ -35,27 +35,21 @@ add_if_pos(H, Rest, Sum) :-
   H > 0 -> Sum is H + Rest;
   Sum is Rest.
 
-sumz_list([], 0).
-sumz_list([H|T], Sum) :-
-  sumz_list(T, Rest),
+tsuma([], 0).
+tsuma([H|T], Sum) :-
+  tsuma(T, Rest),
   add_if_pos(H, Rest, Sum).
-
-tsuma(S, K) :-
-  sumz_list(S, K).
 
 % 3.7 sulieti(S1,S2,R) - duoti išrūšiuoti didėjimo tvarka sąrašai S1 ir S2.
 % Sąrašas R gaunamas suliejus šiuos du sąrašus taip, kad jo elementai eitų didėjimo tvarka. Pavyzdžiui:
 %         ?- sulieti([1,4,11],[2,5,7],R).
 %         R = [1,2,4,5,7,11].
-merge([],L,L).
-merge(L,[],L).
-merge([H1|T1], [H2|T2], L) :- 
-    H1 < H2 -> L = [H1|R], merge(T1,[H2|T2],R) ;
-    H1 > H2 -> L = [H2|R], merge([H1|T1],T2,R) ;
-    L = [H1,H2|R], merge(T1,T2,R).
-
-sulieti(S1, S2, R) :-
-  merge(S1, S2, R).
+sulieti([],L,L).
+sulieti(L,[],L).
+sulieti([H1|T1], [H2|T2], L) :- 
+    H1 < H2 -> L = [H1|R], sulieti(T1,[H2|T2],R) ;
+    H1 > H2 -> L = [H2|R], sulieti([H1|T1],T2,R) ;
+    L = [H1,H2|R], sulieti(T1,T2,R).
 
 % 4.9 posarasis(S1,S2) - sąrašas S2 susideda iš (gal būt ne visų) sąrašo S1 elementų,
 % išdėstytų ta pačia tvarka, kaip ir sąraše S1.
@@ -69,10 +63,7 @@ is_found_in([H| T], H, T1) :-
 is_found_in([_| T], H2, T1) :-
   is_found_in(T, H2, T1).
 
-subset(_, []).
-subset(S1, [H2| T2]) :-
+posarasis(_, []).
+posarasis(S1, [H2| T2]) :-
   is_found_in(S1, H2, NT),
-  subset(NT, T2).
-
-posarasis(S1, S2) :-
-  subset(S1, S2).
+  posarasis(NT, T2).
